ba65c9497946c223270747ec80318afdc69a5542
Gerry Donnelly <donneger33@gmail.com>
2021-02-11 21:22:05 +0000

    Work in progress for Weekly Task 4

--- Week4/Wk4Taskcollatz.py
+++ Week4/Wk4Taskcollatz.py
@@ -1,21 +1,31 @@
 # Program to take positive integer inputs and outputs the successive values of the following calculation.
-# At each step calculate the next value by taking the current value and, if it is even, divide it by two, but if it is odd, multiply it by three and add one.
-# Have the program end if the current value is one.
+# 
+
 
 # Author: Gerry Donnelly
 
+# prepare the variables
 numinput = int(input("Please enter a positive number:"))
-numout = int()
-numlist = []
-while numinput < 0:
+numout = int() #Will use this for the output. 
+numlist = [] # Empty list to be used to hold the numbers for each calculation.
+
+# Take the user input, check if it is a valid integer, i.e. not negative. 
+while numinput < 0: # Cannot have a negative number. 
     numinput = int(input("You have entered a negative number, please enter only a positive number:"))
 
-while numout != 1:
-    if numinput%2 == 0:
-        numout = numinput/2
-        numlist.append(numout)
-    else:
-        numout = (numinput*3)+1
-        numlist.append(numout)
+numlist.append(numinput) # Get here when positive integer is input, add it to the list as the first number. 
 
-print (numlist)
+# Calculation section, as long as the calculation result is not equal to 1 the program checks each succesive result. 
+# if it is even, divide it by two, but if it is odd, multiply it by three and add one. Append each result to the list
+while numout != 1:
+    if numinput%2 == 0: # If value is even divide it by 2 and append to the list. 
+        numout = int(numinput/2)
+        numlist.append(numout)
+        numinput = numout
+    else:
+        numout = int((numinput*3)+1) # if value is odd multiply by 3 and add 1 and add to list. 
+        numlist.append(numout)
+        numinput = numout
+
+# Get here because the calculation has resulted in 1 so loop ends. 
+print (numlist) # Print the resulting list of numbers from the calculation. 
