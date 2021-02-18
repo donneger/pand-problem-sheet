3b648fef39bb4b1370c1b0dae6154b201e4911d1
Gerry Donnelly <donneger33@gmail.com>
2021-02-18 08:04:08 +0000

    Coding work in progress for Weekly Task 5

--- Week5/weekday.py
+++ Week5/weekday.py
@@ -4,9 +4,8 @@
 
 import datetime as dt # Import datetime module to convert system time.
 
-x = (dt.datetime.now()) # Get todays date. 
+today = dt.datetime.now().strftime("%A") # Get todays date and format it to get full day name in string format. 
 
-today = x.strftime("%A") # Convert todays date to string.
 #today = "Sunday" 
 
 # Set up days of week, day and day type. 
@@ -20,11 +19,11 @@ daysofweek = {
     "Sunday" : "Weekend",
 }
 
-for day, type in daysofweek.items(): # Loop thorugh the dictionary to get the days key.
+for day, type in daysofweek.items(): # Loop thorugh the dictionary to get the days key and day type.
     if day == today:                   # Check if days key matches what day it is.
         if type == "Weekday":           # When we get a match check the type of day. 
-            print ("Today is: {}, unfortunately it is a {}.".format(day, type)) # Get here if it is a weekday. 
-            break   # If it is a weekday can break here, no need to go any further. 
-        print ("Today is: {}, waahay it is a {}.".format(day, type)) # If it is not a weekday it must be a weekend day. 
+            print ("Today is: {}, unfortunately it is a {}.".format(day, type))     # Get here if it is a weekday. 
+            break    # If it is a weekday can break here, no need to go any further. 
+        print ("Today is: {}, waahay it is a {}.".format(day, type))    # If it is not a weekday it must be a weekend day. 
         
 
