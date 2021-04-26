f56364bb911803ea4666cb3923d10aa25da99519
Gerry Donnelly <donneger33@gmail.com>
2021-04-26 20:56:48 +0100

    Updating the program to take the name of the input text as an argument on the command line.

--- Week7/es.py
+++ Week7/es.py
@@ -1,15 +1,20 @@
 # Program to count the number of "e" letters in a text file. 
-# This program uses the text of the noverl Moby Dick , source https://www.gutenberg.org/files/2701/old/moby10b.txt
+# This program uses the text of the novel Moby Dick. 
 # The text for counting uses the novel text only, excludes the file intro and etymology.
 
 # Author: Gerry Donnelly
 
-filename = ('MobyDick.txt') 
+import sys # Module to allow arguments / variables to be used in the python runtime environment.
 
-with open(filename, "r") as filetext: # Open the file for reading. 
+with open(sys.argv[1], "r") as filetext: # Open the file entered on the command line, in this case MobyDick.txt.
     readfile = filetext.read()  # Read in the text
     filelen = (len(readfile)) # Calculate the total length of the text. 
     ecount = readfile.count('e') # Count the number of 'e' characters in the text
     
     # Print out the results. Using {:,} to format the numbers output.
-    print("The total numbers of characters (letters) in the text is: {:,}\nThe total count for the letter 'e' is {:,}".format(filelen, ecount))
+    print("\nThe total numbers of characters (letters) in the text is: {:,}\nThe total count for the letter 'e' is {:,}\n".format(filelen, ecount))
+
+    # References:
+    # https://www.gutenberg.org/files/2701/old/moby10b.txt. This is tthe source of the text I used. 
+    # https://www.tutorialsteacher.com/python/sys-module
+
