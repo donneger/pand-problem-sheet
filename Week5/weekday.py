3c39a97da29e699414a34582a2dcb5a76b286fe8
Gerry Donnelly <donneger33@gmail.com>
2021-04-26 17:32:10 +0100

    Addition of references to the program and some minor text updates.

--- Week5/weekday.py
+++ Week5/weekday.py
@@ -1,5 +1,6 @@
-# Program to indicate if current date is a weekday.
-
+# Program to indicate if current date is a weekday. The program checks the current day of the week and indicates if it is a 
+# weekday or a weekend day. 
+# Week 5 Task. 
 # Author: Gerry Donnelly.
 
 import datetime as dt # Import datetime module to convert system time.
@@ -27,3 +28,6 @@ for day, type in daysofweek.items():    # Loop thorugh the dictionary to get the
         print ("\nToday is: {}, waahay it is a {}.\n".format(day, type))    # If it is not a weekday it must be a weekend day. 
         
 
+# References:
+# https://www.w3schools.com/python/python_dictionaries.asp
+# https://docs.python.org/3/library/datetime.html
